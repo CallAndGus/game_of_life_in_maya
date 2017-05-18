a517b26cc75ceca158c4d156632302597bb6c60a
PixarNinja <wesharris505@gmail.com>
2017-05-17 21:00:54 -0600

    Parse mesh and initialize matrices

--- main.py
+++ main.py
@@ -11,31 +11,27 @@ def displayWindow():
     cmds.setParent( '..' )
     cmds.showWindow( window )
 
-def findNeighbors(mesh):
-    # Extract vertices
-    name, range = mesh[0].split('.vtx')
-    start, stop = range.split(':')
+def createGraph(mesh, name):
+    start, stop = (mesh[0].split('.vtx')[1]).split(':')
     start = start.split('[')[1]
     stop = stop.split(']')[0]
-    # Print the name, and the first and last vertices
-    print name
-    print start
-    print stop
+    cmds.select( name )
+    # Extract edges
+    numEdges = cmds.polyEvaluate( e=True )
+    print numEdges
+    # Extract vertices
+    numVerts = cmds.polyEvaluate( v=True )
+    print numVerts
     # Initialize graph
-    row = []
     graph = []
-    i = int(start)
-    while i <= int(stop):
+    for i in range ( 0, numVerts ):
         row = []
-        j = int(start)
-        while j <= int(stop):
+        for j in range ( 0, numEdges ):
             row.append( 0 )
-            j = j + 1
         graph.append( row )
-        i = i + 1
+    print graph
     # Fill graph with vertices
-    i = int(start)
-    while i <= int(stop):
+    for i in range ( int(start), int(stop) ):
         cmds.select( '{0}.vtx[{1}]'.format( name, i ) )
         found = cmds.polyInfo( ve=True )
         print found
@@ -46,16 +42,53 @@ def findNeighbors(mesh):
             if (tmp.isalnum() == True) and (tmp != 'VERTEX'):
                 # Add each alphanumeric string to the graph
                 graph[i][int(tmp)] = 1
-        i = i + 1
     cmds.select( mesh )
     return graph
+
+def initLife(mesh, name, id):
+    matrix = cmds.group( em=True, n='group{0}'.format(id) )
+    start, stop = (mesh[0].split('.vtx')[1]).split(':')
+    start = start.split('[')[1]
+    stop = stop.split(']')[0]
+    for i in range( int(start), int(stop) ):
+        # Find the coordinates of each vertex
+        trans = cmds.pointPosition( '{0}.vtx[{1}]'.format( name, i ) )
+        # Create a cube at each vertex
+        obj = cmds.polyCube()
+        cmds.parent( obj, matrix )
+        cmds.xform( obj, t=(trans[0], trans[1], trans[2]) )
+    return matrix
+
+def scaleMatrix(matrix, factor):
+    children = cmds.listRelatives( matrix, c=True )
+    for obj in children:
+        cmds.xform( obj, s=(factor[0], factor[1], factor[2]) )
+
+##########################
+### Main Functionality ###
+##########################
 
 # Display menu window
 displayWindow()
 
-# Transfer the selected vertices into a graph
+# Store mesh variables
 mesh = cmds.ls( selection=True )
-print mesh
-graph = findNeighbors(mesh)
-print graph
+name = mesh[0].split('.vtx')[0]
+
+# Transfer the selected vertices into a graph
+graph = createGraph(mesh, name)
+for i in graph:
+    print i
+    
+# Initialize rendering
+matrixA = initLife(mesh, name, 'MatrixA')
+matrixB = initLife(mesh, name, 'MatrixB')
+
+# Scale both matrices to 0
+factorDead = (0.0, 0.0, 0.0)
+factorAlive = (1.0, 1.0, 1.0)
+scaleMatrix(matrixA, factorDead)
+scaleMatrix(matrixB, factorDead)
+
+# Load seed pattern
 
